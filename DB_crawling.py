6243aadb62f6c14799325e6e892e22fd0df7648c
ujin <yujin5836@gmail.com>
2022-10-24 17:30:01 +0900

    Update : file save & copy
    update code for copy BD

--- DB_crawling.py
+++ DB_crawling.py
@@ -1,25 +1,36 @@
 import requests
 from urllib.request import urlretrieve
 import time
+import sys
 from apscheduler.schedulers.background import BlockingScheduler
+import csv 
+import pandas as pd
+
+import shutil
 
 
 def DB():
     url = "https://www.bitcoinabuse.com/api/download/1d?api_token=9GyW3puHaW635XsQgeNlvIJaSohBDD20xqpTSu2w"
-
     resp = requests.get(url)
 
     if resp.status_code == 200 : 
-        print("hi")
-        urlretrieve(url, "./BC_DB.csv")
 
+        print("1")
+        urlretrieve(url, "./BC_DB.csv")
+        
+        from_file_path = './BC_DB.csv' # 복사할 파일
+        to_file_path = './abuse_DB.csv' # 복사 위치 및 파일 이름 지정
+        shutil.copyfile(from_file_path, to_file_path) 
+
+        print("2")
 
 
 def backgroundScheduler():
     scheduler = BlockingScheduler(timezone='Asia/Seoul')
-    scheduler.add_job(DB, 'cron', hour=20)
+    scheduler.add_job(DB, 'cron', second=10)
     scheduler.start()
 
 if __name__ == '__main__':
+    print("0")
     backgroundScheduler()
-
+    
---
+++ config.py
@@ -0,0 +1,4 @@
+
+Auth = {
+    'token':'9GyW3puHaW635XsQgeNlvIJaSohBDD20xqpTSu2w'
+}
